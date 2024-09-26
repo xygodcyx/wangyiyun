09de75acd785d0f6adf70db868235eff77ab674b
xygodcyx <1323943635@qq.com>
2024-09-26 19:05:18 +0800

    update

--- client/src/request/request.ts
+++ client/src/request/request.ts
@@ -3,6 +3,5 @@ export const request = axios.create({
   // baseURL: 'http://localhost:80/', //开发环境
   // baseURL: ' http://yuandifuhuo.natapp1.cc/', //生产环境
   baseURL: '//music-server.quhou.top/', //免费的生产环境
-
   timeout: 6000
 });
